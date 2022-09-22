2aa3b7dfff52d7726e2bc324653a7c5e4801514e
유정환 <motorola4714@gmail.com>
2022-09-22 15:15:47 +0900

    지도 좌표 이동

--- js/hnsevent.js
+++ js/hnsevent.js
@@ -811,3 +811,25 @@ function layerInfo(){
     window.open( "./layerinfo.html", "Child", "width=815, height=1000px, top=160, left=480");
 }
 
+function mapsetting(){
+    if (base == 1) { // 위성지도
+        map.removeLayer(hybridLayer1);
+        map.removeLayer(hybridLayer2);
+        map.removeLayer(baseLayer);
+
+        map.setView([35.0686, 128.994], 11);
+
+        map.addLayer(hybridLayer1);
+        map.addLayer(hybridLayer2);
+
+    } else { // 일반지도
+        map.removeLayer(baseLayer);
+        map.removeLayer(hybridLayer1);
+        map.removeLayer(hybridLayer2);
+
+        map.setView([35.0686, 128.994], 11);
+
+        map.addLayer(baseLayer);
+    }
+}
+
