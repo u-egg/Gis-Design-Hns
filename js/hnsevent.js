424a66a7b7b8503448f0bcc6cdead5ad13e0642a
유정환 <motorola4714@gmail.com>
2022-09-27 14:51:01 +0900

    데이트피커 오류 해결

--- js/hnsevent.js
+++ js/hnsevent.js
@@ -66,30 +66,6 @@ function content_silde() {
     }
 }
 
-
-
-
-//datepicker
-function datepicker(){
-$.datepicker.setDefaults({
-    dateFormat: 'yy-mm-dd',
-    prevText: '이전 달',
-    nextText: '다음 달',
-    monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-    monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-    dayNames: ['일', '월', '화', '수', '목', '금', '토'],
-    dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-    dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
-    showMonthAfterYear: true,
-    yearSuffix: '년'
-  });
-}
-
-  $(function () {
-    $('.datepicker').datepicker({
-    });
-});
-
 function classSetTime() {
 
 var objTime = $('.setTime');
@@ -852,6 +828,7 @@ function mapsetting(){ // 좌표이동
 
         map.setView([35.0686, 128.994], 11);
 
+        addMaker();
         map.addLayer(hybridLayer1);
         map.addLayer(hybridLayer2);
 
